49216fc2a91b876882f6ada39d80db8b2e917bb5
Heyna-log <rhskgus048@naver.com>
2023-03-27 15:31:02 +0900

    jsp에 POST 방식으로 데이터 전달

--- src/main/java/com/board/controller/IndexController.java
+++ src/main/java/com/board/controller/IndexController.java
@@ -3,7 +3,9 @@ package com.board.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/")
@@ -11,9 +13,26 @@ public class IndexController {
 
 	@GetMapping()
 	public String Index(Model model) {
+		
+		String returnUrl = "index";
+		
 		model.addAttribute("data", "Hi");
 		
-		System.out.println("start");
-		return "index";
+		System.out.println(returnUrl);
+		return returnUrl;
+	}
+	
+	@PostMapping("/result")
+	public String result(Model model, 
+			@RequestParam("value1") String val1,
+			@RequestParam("value2") String val2) {
+		
+		String returnUrl = "result";
+		
+		model.addAttribute("param1", val1);
+		model.addAttribute("param2", val2);
+		
+		System.out.println(returnUrl);
+		return returnUrl;
 	}
 }
--- src/main/webapp/WEB-INF/view/index.jsp
+++ src/main/webapp/WEB-INF/view/index.jsp
@@ -9,5 +9,11 @@
 <body>
 	<h1>INDEX</h1>
 	<h2>${data}</h2>
+	<form action="/result" method="post">
+		<span>Enter a value1 : </span><input type="text" name="value1"><br>
+		<span>Enter a value2 : </span><input type="text" name="value2"><br>
+		<br>
+		<button type="submit">Check Parameters !</button>
+	</form>
 </body>
 </html>
