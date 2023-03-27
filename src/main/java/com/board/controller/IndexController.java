61f5fa1ce57515b8bda2faa75045b131f5f3c255
Heyna-log <rhskgus048@naver.com>
2023-03-27 15:52:48 +0900

    jsp에 데이터 전달 - ModelAndView 사용

--- src/main/java/com/board/controller/IndexController.java
+++ src/main/java/com/board/controller/IndexController.java
@@ -8,34 +8,51 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/")
 public class IndexController {
 
 	@GetMapping()
-	public String Index(HttpServletRequest request) {
+	public ModelAndView Index() {
 		
 		String returnUrl = "index";
 		
-		request.setAttribute("data", "Hi"); // == model.addAttribute
+		// 생성자 메소드
+		// ModelAndView(String viewName, String name, Object value);
+		ModelAndView mv = new ModelAndView(returnUrl, "data", "Hi");
 		
 		System.out.println(returnUrl);
-		return returnUrl;
+		
+		// ModelAndView 객체 반환
+		return mv;
 	}
 	
 	@PostMapping("/result")
-	public String result(HttpServletRequest request) {
+	public ModelAndView result(HttpServletRequest request) {
 		
 		String returnUrl = "result";
 		
 		String param1 = request.getParameter("value1");
 		String param2 = request.getParameter("value2");
 		
+		// ModelAndView 객체 선언 및 생성
+		ModelAndView mv = new ModelAndView();
+		
 		request.setAttribute("param1", param1);
 		request.setAttribute("param2", param2);
 		
+		// view의 이름 설정
+		mv.setViewName(returnUrl);
+		
+		// 데이터 저장(데이터이름, 데이터)
+		mv.addObject("param1", param1);
+		mv.addObject("param2", param2);
+		
 		System.out.println(returnUrl);
-		return returnUrl;
+		
+		// ModelAndView 객체 반환
+		return mv;
 	}
 }
