54dfc15140d821861aa995a02921e1cae910b2d3
Heyna-log <rhskgus048@naver.com>
2023-03-27 15:16:29 +0900

    DB 연동 전 DataSourceAutoConfiguration 제외 설정

--- src/main/java/com/board/BoardApplication.java
+++ src/main/java/com/board/BoardApplication.java
@@ -2,8 +2,9 @@ package com.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class) // DB 연동하기 전이므로 DataSourceAutoConfiguration 제외 설정
 public class BoardApplication {
 
 	public static void main(String[] args) {
