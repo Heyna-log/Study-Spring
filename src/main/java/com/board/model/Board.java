2b0f0f093c30cd812ac23c2fbbeb872085f3b92c
Heyna-log <rhskgus048@naver.com>
2023-06-27 11:07:04 +0900

    @Data 교체

--- src/main/java/com/board/model/Board.java
+++ src/main/java/com/board/model/Board.java
@@ -1,11 +1,12 @@
 package com.board.model;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
-@Data
-//@Getter
-//@Setter
-//@ToString
+@Getter
+@Setter
+@ToString
 public class Board {
 	private String id;
 	private String pw;
